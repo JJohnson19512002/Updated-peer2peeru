a552de63d9bc8370c3fe75addc6d8b7f2c90ace4
JJohnson19512002 <justin.johnson1@washburn.edu>
2025-05-14 16:15:40 -0700

    Add files via upload

--- database/insert_sample.sql
+++ database/insert_sample.sql
@@ -4,3 +4,24 @@ VALUES ('Topeka', 'Kansas', 'USA');
 
 Insert into universities (UniversityID, Name, locationId)
 VALUES (101, 'Washburn University', 1);
+
+INSERT INTO Categories (CategoryName) VALUES
+  ('Textbooks'),
+  ('Electronics'),
+  ('Backpacks & Bags'),
+  ('Furniture'),
+  ('Clothing & Apparel'),
+  ('Footwear'),
+  ('Sports Equipment'),
+  ('Art & Craft Supplies'),
+  ('Lab & Science Equipment'),
+  ('Health & Wellness'),
+  ('Office Supplies'),
+  ('Snacks & Beverages'),
+  ('Cleaning Supplies'),
+  ('Software & Apps'),
+  ('School Spirit Merchandise'),
+  ('Musical Instruments'),
+  ('Dorm Decor'),
+  ('Bedding & Linens'),
+  ('Travel Accessories');
